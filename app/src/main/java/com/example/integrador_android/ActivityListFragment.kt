481fe0da5354e6a01be4d546642b3b6a60f21f5e
tomasdiazMELI <tomas.diaz@mercadolibre.com>
2021-11-04 12:25:52 -0300

    added imports recyclerview?

--- app/src/main/java/com/example/integrador_android/ActivityListFragment.kt
+++ app/src/main/java/com/example/integrador_android/ActivityListFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.integrador_android.adapter.ItemAdapter
+import com.example.integrador_android.data.Datasource
 import com.example.integrador_android.databinding.FragmentActivityListBinding
 
 /**
@@ -16,6 +20,9 @@ class ActivityListFragment : Fragment() {
 
     private var _binding: FragmentActivityListBinding? = null
     private val binding get() = _binding!!
+    private var participants: Int = 0
+
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +34,16 @@ class ActivityListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentActivityListBinding.inflate(inflater, container, false)
-        // Inflate the layout for this fragment
-
+        // Inflate the layout for this
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = binding.recyclerView
+        val myDataSet = Datasource().loadActivities()
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = ItemAdapter(requireContext(), myDataSet)
     }
 
     override fun onDestroyView() {
