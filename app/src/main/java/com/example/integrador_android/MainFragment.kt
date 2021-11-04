fa9c1860bb10ca54951ab23bb36dead7a2491309
tomasdiazMELI <tomas.diaz@mercadolibre.com>
2021-11-04 16:05:29 -0300

    added share preferences participants value

--- app/src/main/java/com/example/integrador_android/MainFragment.kt
+++ app/src/main/java/com/example/integrador_android/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.example.integrador_android
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,8 +9,7 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.integrador_android.databinding.FragmentMainBinding
 import androidx.appcompat.app.AppCompatActivity
-
-
+import androidx.core.widget.doOnTextChanged
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -54,10 +54,36 @@ class MainFragment : Fragment() {
         }
 
         binding.btnStart.setOnClickListener{
-            val action = MainFragmentDirections.actionMainFragmentToActivityListFragment()
+            val isValid: Boolean = validateResponse(binding.inputParticipants.text.toString())
 
-            findNavController().navigate(action)
+            if(isValid){
+                val participantsValue = binding.inputParticipants.text.toString().toInt()
+                val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+
+                sharedPref?.let {
+                    with(it.edit()){
+                        putInt(getString(R.string.saved_participants_key), participantsValue)
+                        apply()
+                    }
+                }
+
+                val action = MainFragmentDirections.actionMainFragmentToActivityListFragment()
+                findNavController().navigate(action)
+            }
         }
+    }
+
+    private fun validateResponse(text: String) : Boolean {
+        var response = true
+        if(text.isNullOrBlank()){
+            getString(R.string.error_empty).also { binding.inputParticipants.error = it }
+            response = false
+        } else if (text.toInt() == 0) {
+            getString(R.string.error_zero).also { binding.inputParticipants.error = it }
+            response = false
+        }
+
+        return response
     }
 
     override fun onDestroyView() {
