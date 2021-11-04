9b38b43b2929f84412ab3bc5ff490b382031e9f6
Carolina Olivares <carolina.olivares@mercadolibre.com>
2021-11-04 12:46:41 -0300

    implementation of retrofit

--- app/src/main/java/com/example/integrador_android/ActivityClass.kt
+++ app/src/main/java/com/example/integrador_android/ActivityClass.kt
@@ -1,6 +1,6 @@
 package com.example.integrador_android
 
-class Activity {
+class ActivityClass {
     lateinit var name : String
     lateinit var price : String
     var participants : Int = 0
--- app/src/main/java/com/example/integrador_android/MainActivity.kt
+++ app/src/main/java/com/example/integrador_android/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.integrador_android.databinding.ActivityMainBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
+    private val activities = mutableListOf<ActivityClass>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,6 +31,15 @@ class MainActivity : AppCompatActivity() {
     // Permite controlar la navegación hacía atras
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
+    // permite conectar con la api breed
+    private fun getRetroFit() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
 
